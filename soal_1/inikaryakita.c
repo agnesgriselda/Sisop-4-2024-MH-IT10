9b6a7b5b5a00e64b5e66fe6a2a9f79b8519d5fde
araapk <150429708+araapk@users.noreply.github.com>
2024-05-21 22:03:07 +0700

    Update inikaryakita.c

--- soal_1/inikaryakita.c
+++ soal_1/inikaryakita.c
@@ -10,14 +10,126 @@
 #include <unistd.h>
 #include <dirent.h>
 
+// Path root dari filesystem
 static const char *root_path = "/home/pikaa/soal1-wm/portofolio";
 
+// Prototipe fungsi
+int reverse_file_content(const char *path);
+
+// Fungsi untuk mendapatkan seluruh path
+static void get_full_path(char *full_path, const char *path) {
+    snprintf(full_path, 256, "%s%s", root_path, path);
+}
+// Fungsi untuk mendapatkan atribut file
+static int fs_getattr(const char *path, struct stat *stbuf) {
+    int result;
+    char full_path[256];
+    get_full_path(full_path, path);
+
+    result = lstat(full_path, stbuf);
+    if (result == -1)
+        return -errno;
+
+    return 0;
+}
+// Fungsi untuk mengubah permission file
+static int fs_chmod(const char *path, mode_t mode) {
+    int result;
+    char full_path[256];
+    get_full_path(full_path, path);
+
+    result = chmod(full_path, mode);
+    if (result == -1)
+        return -errno;
+
+    return 0;
+}
+// Fungsi untuk menambahkan watermark
 int add_watermark(const char *path) {
     char command[256];
     snprintf(command, sizeof(command), "convert %s -gravity SouthEast -pointsize 36 -draw \"text 10,10 'inikaryakita.id'\" %s", path, path);
     return system(command);
 }
-// Fungsi untuk membalik isi file
+// Fungsi untuk memindahkan file dan menambahkan watermark di folder gallery
+static int fs_rename(const char *from, const char *to) {
+    int result;
+    char full_from[256];
+    char full_to[256];
+    get_full_path(full_from, from);
+    get_full_path(full_to, to);
+
+    result = rename(full_from, full_to);
+    if (result == -1)
+        return -errno;
+
+    if (strncmp(to, "/gallery/", 9) == 0) {
+        add_watermark(full_to);
+    }
+
+    return 0;
+}
+// Fungsi untuk menulis ke file dan membalik isinya di folder bahaya dengan prefix text
+static int fs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+    (void) fi;
+    int fd;
+    int result;
+    char full_path[256];
+    get_full_path(full_path, path);
+
+    fd = open(full_path, O_WRONLY);
+    if (fd == -1)
+        return -errno;
+
+    result = pwrite(fd, buf, size, offset);
+    if (result == -1)
+        result = -errno;
+
+    close(fd);
+
+    if (strncmp(path, "/bahaya/test", 12) == 0) {
+        reverse_file_content(full_path);
+    }
+
+    return result;
+}
+// Fungsi untuk membuat direktori
+static int fs_mkdir(const char *path, mode_t mode) {
+    int result;
+    char full_path[256];
+    get_full_path(full_path, path);
+
+    result = mkdir(full_path, mode);
+    if (result == -1)
+        return -errno;
+
+    return 0;
+}
+// Fungsi untuk membaca isi direktori
+static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
+    (void) offset;
+    (void) fi;
+    char full_path[256];
+    get_full_path(full_path, path);
+
+    DIR *dp;
+    struct dirent *entry;
+    dp = opendir(full_path);
+    if (dp == NULL)
+        return -errno;
+
+    while ((entry = readdir(dp)) != NULL) {
+        struct stat st;
+        memset(&st, 0, sizeof(st));
+        st.st_ino = entry->d_ino;
+        st.st_mode = entry->d_type << 12;
+        if (filler(buf, entry->d_name, &st, 0))
+            break;
+    }
+
+    closedir(dp);
+    return 0;
+}
+// Fungsi untuk mereverse isi file
 int reverse_file_content(const char *path) {
     FILE *file = fopen(path, "r+");
     if (!file) return -errno;
@@ -47,123 +159,20 @@ int reverse_file_content(const char *path) {
 
     return 0;
 }
-// Mendapatkan atribut file
-static int fs_getattr(const char *path, struct stat *stbuf) {
-    int result;
-    char full_path[256];
-    snprintf(full_path, sizeof(full_path), "%s%s", root_path, path);
-
-    result = lstat(full_path, stbuf);
-    if (result == -1)
-        return -errno;
-
-    return 0;
-}
-// Mengubah permission file
-static int fs_chmod(const char *path, mode_t mode) {
-    int result;
-    char full_path[256];
-    snprintf(full_path, sizeof(full_path), "%s%s", root_path, path);
-
-    result = chmod(full_path, mode);
-    if (result == -1)
-        return -errno;
-
-    return 0;
-}
-// Memindahkan file dan menambahkan watermark jika di folder gallery
-static int fs_rename(const char *from, const char *to) {
-    int result;
-    char full_from[256];
-    char full_to[256];
-    snprintf(full_from, sizeof(full_from), "%s%s", root_path, from);
-    snprintf(full_to, sizeof(full_to), "%s%s", root_path, to);
-
-    result = rename(full_from, full_to);
-    if (result == -1)
-        return -errno;
-
-    if (strncmp(to, "/gallery/", 9) == 0) {
-        add_watermark(full_to);
-    }
-
-    return 0;
-}
-// Menulis ke file dan membalik isinya jika di folder bahaya dengan prefix test
-static int fs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
-    (void) fi;
-    int fd;
-    int result;
-    char full_path[256];
-    snprintf(full_path, sizeof(full_path), "%s%s", root_path, path);
-
-    fd = open(full_path, O_WRONLY);
-    if (fd == -1)
-        return -errno;
-
-    result = pwrite(fd, buf, size, offset);
-    if (result == -1)
-        result = -errno;
-
-    close(fd);
-
-    if (strncmp(path, "/bahaya/test", 12) == 0) {
-        reverse_file_content(full_path);
-    }
-
-    return result;
-}
-// Membuat direktori
-static int fs_mkdir(const char *path, mode_t mode) {
-    int result;
-    char full_path[256];
-    snprintf(full_path, sizeof(full_path), "%s%s", root_path, path);
-
-    result = mkdir(full_path, mode);
-    if (result == -1)
-        return -errno;
-
-    return 0;
-}
-// Membaca isi direktori
-static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
-    (void) offset;
-    (void) fi;
-    char full_path[256];
-    snprintf(full_path, sizeof(full_path), "%s%s", root_path, path);
-
-    DIR *dp;
-    struct dirent *entry;
-    dp = opendir(full_path);
-    if (dp == NULL)
-        return -errno;
-
-    while ((entry = readdir(dp)) != NULL) {
-        struct stat st;
-        memset(&st, 0, sizeof(st));
-        st.st_ino = entry->d_ino;
-        st.st_mode = entry->d_type << 12;
-        if (filler(buf, entry->d_name, &st, 0))
-            break;
-    }
-
-    closedir(dp);
-    return 0;
-}
-// Membaca isi file
+// Fungsi untuk membaca isi file
 static int fs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
     (void) fi;
     int fd;
     int result;
     char full_path[256];
-    snprintf(full_path, sizeof(full_path), "%s%s", root_path, path);
+    get_full_path(full_path, path);
 
     fd = open(full_path, O_RDONLY);
     if (fd == -1)
         return -errno;
 
     if (strncmp(path, "/bahaya/test", 12) == 0) {
-        // Baca filenya
+        // Baca seluruh isi file
         lseek(fd, 0, SEEK_END);
         size_t length = lseek(fd, 0, SEEK_CUR);
         lseek(fd, 0, SEEK_SET);
@@ -176,14 +185,14 @@ static int fs_read(const char *path, char *buf, size_t size, off_t offset, struc
 
         pread(fd, file_buf, length, 0);
 
-        // Reverse filenya
+        // Reverse isi file
         for (size_t i = 0; i < length / 2; ++i) {
             char temp = file_buf[i];
             file_buf[i] = file_buf[length - 1 - i];
             file_buf[length - 1 - i] = temp;
         }
 
-        // Copy reverse file
+        // Copy isi file yang telah di reverse ke buffer
         size_t copy_size = (offset < length) ? (length - offset < size ? length - offset : size) : 0;
         memcpy(buf, file_buf + offset, copy_size);
 
@@ -199,7 +208,7 @@ static int fs_read(const char *path, char *buf, size_t size, off_t offset, struc
         return result;
     }
 }
-
+// Operasi filesystem
 static struct fuse_operations fs_operations = {
     .getattr = fs_getattr,
     .chmod = fs_chmod,
